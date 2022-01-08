a6d7ea192c7cb2452ad6202ae437fa29e518033c
gursimran18 <kaurgursimran9918@gmail.com>
2022-01-08 14:09:47 +0530

    setup for gallery done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './views/homepage';
-import ShareRotaryStory from './components/shareRotaryStory';
 import Gallery from './components/gallery'
-import Image from './image'
-import Blog from './components/blogs';
-import TestPage from './components/testpage';
 
 
 function App() {
@@ -15,10 +11,6 @@ function App() {
         <switch>
           <Route path="/" exact component={Home} />
           <Route path="/gallery" component={Gallery} />
-          {/* <Route path="/story" component={ShareRotaryStory} /> */}
-          {/* <Route path="/upload_image_admin_luffy_6972" component={Image} /> */}
-          {/* <Route path="/blog" component={Blog} /> */}
-          {/* <Route path="/test" component={TestPage} /> */}
         </switch>
       </Router>
       
